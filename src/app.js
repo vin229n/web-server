c916a5b620b72e65afdec832e8587cc6b971bd67
Vinayak Patil <vinayak.p@BRILLIO.COM>
2019-09-04 16:34:46 +0530

    restart

--- src/app.js
+++ src/app.js
@@ -1,22 +1,46 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates')
+
+//Setup handlers engine and views location
+app.set('view engine', 'hbs')
+app.set('views',viewsPath)
+
+//SetUp static directory to serve 
 app.use(express.static(path.join(__dirname,'../public')))
 
 app.get('',(req,res)=>{
-    res.send('hello!')
+    res.render('index',{
+        title:'Weather app',
+        name: 'Vinayak'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About me',
+        name: 'Vinayak'
+    })
 })
 
 app.get('/help',(req,res)=>{
-    res.send({
-        name:'Vinayak',
-        age: 22
+    res.render('help',{
+        title:'need help?',
+        name: 'Vinayak'
     })
 })
+
+
 
 
 app.listen(3000,()=>{
     console.log("Serever running on port 3000")
 })
+
